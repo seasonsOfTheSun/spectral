5a4e96897d0e1a19d7b5d4b0be37f10272bd358c
Joel Hancock <joel@menchelab.com>
2022-11-15 14:26:28 +0100

    adjusted for sphere layout, and convenient file naming

--- draw_for_vrnetzer.py
+++ draw_for_vrnetzer.py
@@ -6,7 +6,6 @@ import sphere
 import sys 
 attempt = sys.argv[1]
 
-
 df = pd.read_csv("data/intermediate/gene_evector_entropy.csv", index_col=[0,1])
 df["kl"] = df["cross_to_normal"] - df["empirical_entropy"]
 
@@ -16,14 +15,11 @@ for i,v in df["kl"].iteritems():
     if not np.isnan(v):
         G.add_edge(evector, gene, weight=np.exp(-v))
 
-
-
 pos = sphere.spherical_spring_layout(G)
 pos_df = pd.DataFrame(pos).T
 
-
 for i in range(3):
-    pos_df[i] = (pos_df[i] - (-1))/2
+    pos_df[i] = (pos_df[i] - (-1.01))/2.02
 
 #for i in range(3):
 #    pos_df[i] = (pos_df[i] - pos_df[i].min())/(pos_df[i].max() - pos_df[i].min())
@@ -40,15 +36,15 @@ pos_df["alpha"] = 100
 
 pos_df = pos_df[[0,1,2,"r","g","b","alpha","type"]]
 
-pos_df.to_csv(f"node_attempt_{attempt}.csv", header=None, index=None)
+pos_df.to_csv(f"for_vrnetzer/node_attempt_{attempt}.csv", header=None, index=None)
 
 convert_label = {v:i for i,v in enumerate(pos_df.index)}
 link_df = pd.DataFrame(G.edges()).applymap(convert_label.get)
 link_df.columns=["source", "target"]
 
-#link_df["r"] = 255#np.where
-#link_df["g"] = 0#np.where
-#link_df["b"] = 0#np.where
-#link_df["alpha"] = 50#np.where
+link_df["r"] = 255#np.where
+link_df["g"] = 0#np.where
+link_df["b"] = 0#np.where
+link_df["alpha"] = 50#np.where
 
-link_df.to_csv(f"link_attempt_{attempt}.csv", header=None, index=None)
+link_df.to_csv(f"for_vrnetzer/link_attempt_{attempt}.csv", header=None, index=None)
