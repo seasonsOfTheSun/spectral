13548931a3212d9dad72010f08e9c049e981a873
Joel Hancock <joel@menchelab.com>
2022-11-18 12:25:00 +0100

    still not loading flawlessly; adjusted various things to improve sphere layout

--- draw_for_vrnetzer.py
+++ draw_for_vrnetzer.py
@@ -13,19 +13,19 @@ G = nx.Graph()
 for i,v in df["kl"].iteritems():
     evector,gene = i
     if not np.isnan(v):
-        G.add_edge(evector, gene, weight=np.exp(-v))
+        G.add_edge(evector, gene, weight=np.exp(-v**2))
 
 pos = sphere.spherical_spring_layout(G)
 pos_df = pd.DataFrame(pos).T
 
 for i in range(3):
-    pos_df[i] = (pos_df[i] - (-1.01))/2.02
-
-#for i in range(3):
-#    pos_df[i] = (pos_df[i] - pos_df[i].min())/(pos_df[i].max() - pos_df[i].min())
+    pos_df[i] = (pos_df[i] - (-1.01))/2.1
 
 e_or_g = pos_df.index.map(lambda x: type(x)==np.int)
-pos_df["type"] = np.where(e_or_g, "Eigenvector", "Gene")
+pos_df.index = [("Eigenvector-"+str(i) if type(i) is int else i) for i in pos_df.index]
+
+
+pos_df["type"] = pos_df.index #np.where(e_or_g, "Eigenvector", "Gene")
 
 e_color = [150,150,0]
 g_color = [0,0,255]
@@ -38,13 +38,15 @@ pos_df = pos_df[[0,1,2,"r","g","b","alpha","type"]]
 
 pos_df.to_csv(f"for_vrnetzer/node_attempt_{attempt}.csv", header=None, index=None)
 
+
+
 convert_label = {v:i for i,v in enumerate(pos_df.index)}
-link_df = pd.DataFrame(G.edges()).applymap(convert_label.get)
+link_df = pd.DataFrame(G.edges()).applymap(lambda i:("Eigenvector-"+str(i) if type(i) is int else i)).applymap(convert_label.get)
 link_df.columns=["source", "target"]
 
 link_df["r"] = 255#np.where
 link_df["g"] = 0#np.where
 link_df["b"] = 0#np.where
-link_df["alpha"] = 50#np.where
+link_df["alpha"] = 1+int(254*np.random.rand())#np.where
 
 link_df.to_csv(f"for_vrnetzer/link_attempt_{attempt}.csv", header=None, index=None)
